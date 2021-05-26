44318706b01d1a0ecfe38135236326cac267aba9
codren <tkdrms0291@naver.com>
2021-05-26 19:12:47 +0900

    custom_action

--- django_practice/order/admin.py
+++ django_practice/order/admin.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
+from django.db.models import Q
+from django.db import transaction
+from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.contenttypes.models import ContentType
 from django.utils.html import format_html
 from .models import Order
+from product.models import Product
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     list_display = ('user', 'product', 'styled_status')
+    change_list_template = 'admin/order_change_list.html'
+
+    def refund(modeladmin, request, queryset):      
+        with transaction.atomic():                 
+            qs = queryset.filter(~Q(status='환불'))    
+            ct = ContentType.objects.get_for_model(queryset.model)
+            for obj in qs:
+                product = Product.objects.get(pk=obj.product.id)
+                if obj.product.stock == product.stock:
+                    obj.product.stock += obj.quantity
+                    obj.product.save()
+                else:
+                    product.stock += obj.quantity
+                    product.save()
+
+                LogEntry.objects.log_action(       
+                    user_id = request.user.id,
+                    content_type_id = ct.pk,
+                    object_id = obj.pk,
+                    object_repr = '주문 환불',
+                    action_flag = CHANGE,
+                    change_message = '주문 환불'
+                )
+            qs.update(status='환불')   
+                     
+    refund.short_description = '환불'  
+
+    actions = [ refund ]
 
     def styled_status(self, obj):
         if obj.status == '환불':
