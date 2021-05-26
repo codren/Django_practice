f297e72be91a3b40ff900284f0f294d2d5e58971
codren <tkdrms0291@naver.com>
2021-05-26 22:54:12 +0900

    list_custom_button

--- django_practice/order/admin.py
+++ django_practice/order/admin.py
@@ -8,7 +8,7 @@ from .models import Order
 from product.models import Product
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status',)
-    list_display = ('user', 'product', 'styled_status')
+    list_display = ('user', 'product', 'styled_status', 'action')
     change_list_template = 'admin/order_change_list.html'
 
     def refund(modeladmin, request, queryset):      
@@ -32,11 +32,22 @@ class OrderAdmin(admin.ModelAdmin):
                     action_flag = CHANGE,
                     change_message = '주문 환불'
                 )
-            qs.update(status='환불')   
-                     
+            qs.update(status='환불')      
     refund.short_description = '환불'  
 
     actions = [ refund ]
+
+
+
+    def action(self, obj):
+        if obj.status != '환불':
+            return format_html(f'<input type = "button" value = "환불"\
+                                onclick = "order_refund_submit({obj.id})"\
+                                class = "btn btn-primary btn-sm">')
+        else:
+            return ''
+
+
 
     def styled_status(self, obj):
         if obj.status == '환불':
@@ -45,16 +56,37 @@ class OrderAdmin(admin.ModelAdmin):
             return format_html(f'<span style="color:green">{obj.status}</span>')
         return obj.status
     
+
+
     def changelist_view(self, request, extra_context=None):
-        extra_context = {'title': '주문 목록'}   
+        extra_context = {'title': '주문 목록'}  
+        if request.method == 'POST':
+            with transaction.atomic():  
+                obj_id = request.POST.get('obj_id')   
+                if obj_id:            
+                    qs = Order.objects.filter(pk=obj_id)   
+                    ct = ContentType.objects.get_for_model(qs.model)
+                    for obj in qs:
+                        obj.product.stock += obj.quantity
+                        obj.product.save()
+                        LogEntry.objects.log_action(       
+                            user_id = request.user.id,
+                            content_type_id = ct.pk,
+                            object_id = obj.pk,
+                            object_repr = '주문 환불',
+                            action_flag = CHANGE,
+                            change_message = '주문 환불')
+                    qs.update(status='환불') 
+
         return super().changelist_view(request, extra_context)
+
+
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         if object_id:
             order = Order.objects.get(pk=object_id)
             extra_context = {'title': f'{order.user.email}의 {order.product.name} 수정하기'}
         return super().changeform_view(request, object_id, form_url, extra_context)  
-
     styled_status.short_description = '상태'
 
 admin.site.register(Order, OrderAdmin)
