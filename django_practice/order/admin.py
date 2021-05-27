a9f250f2c03fba98a7c5ed95dfc7bebe3d711fe2
codren <tkdrms0291@naver.com>
2021-05-27 09:45:58 +0900

    changeform_button_remove

--- django_practice/order/admin.py
+++ django_practice/order/admin.py
@@ -10,6 +10,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     list_display = ('user', 'product', 'styled_status', 'action')
     change_list_template = 'admin/order_change_list.html'
+    change_form_template = 'admin/order_change_form.html'
 
     def refund(modeladmin, request, queryset):      
         with transaction.atomic():                 
@@ -86,6 +87,8 @@ class OrderAdmin(admin.ModelAdmin):
         if object_id:
             order = Order.objects.get(pk=object_id)
             extra_context = {'title': f'{order.user.email}의 {order.product.name} 수정하기'}
+            extra_context['show_save_and_add_another'] = False
+            extra_context['show_save_and_continue'] = False
         return super().changeform_view(request, object_id, form_url, extra_context)  
     styled_status.short_description = '상태'
 
